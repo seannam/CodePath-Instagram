0db6f7757fd1e0ece0389d23a4734e64eec7511b
Sean Nam <namsangwoo1@gmail.com>
2017-03-07 21:45:07 -0800

    Implemented user sign up - creates PFUser

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "instagram"
+            configuration.clientKey = nil
+            configuration.server = "https://instagram-parse-codepath.herokuapp.com/parse"
+        }))
+        
         return true
     }
 
--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onLoginBUtton(_ sender: Any) {
+    }
 
+    @IBAction func onSignUpButton(_ sender: Any) {
+        let newUser = PFUser()
+        
+        print(usernameTextField.text!)
+        print(passwordTextField.text!)
+        
+//        let username = "snam@test.com"
+//        let password = "123456789"
+        
+//        print(username)
+//        print(password)
+        
+        newUser.username = usernameTextField.text!
+        newUser.email = usernameTextField.text!
+        newUser.password = passwordTextField.text!
+        
+//        newUser.username = username
+//        newUser.email = username
+//        newUser.password = password
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("[DEBUG] built user with email: \(newUser.email!)")
+            } else{
+                print("[DEBUG] failed to create user")
+                print(error?.localizedDescription)
+            }
+        }
+//        
+//        newUser.signUpInBackground { (success: Bool, error: Error?) -> Void in
+//            if success {
+//                print("[DEBUG] built user with email: \(newUser.email!)")
+//            } else {
+//                print("[DEBUG] failed to create user")
+//                print(error!.localizedDescription)
+//            }
+//        }
+    }
 }
 
